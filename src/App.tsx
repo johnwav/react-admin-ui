7143ef7b0866d775b143b3c392eb4d66e88e5555
Johnwav <johnwav@users.noreply.github.com>
2023-09-04 21:40:48 +0100

    react router init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import Home from "./pages/home/home";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Users from "./pages/users/Users";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "users",
+      element: <Users />,
+    },
+    {
+      path: "products",
+      element: <Products />,
+    },
+  ]);
+
   return (
     <div>
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
