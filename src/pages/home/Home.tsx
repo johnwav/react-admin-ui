f4552aca4a23b5606ec44da2fa384b497a876507
Johnwav <johnwav@users.noreply.github.com>
2023-09-11 23:45:01 +0100

    box

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -10,9 +10,9 @@ const Home = () => {
     <div className="box box4">box4</div>
     <div className="box box5">box5</div>
     <div className="box box6">box6</div>
-    <div className="box box">box</div>
-    <div className="box box">box</div>
-    <div className="box box">box</div>
+    <div className="box box7">box7</div>
+    <div className="box box8">box8</div>
+    <div className="box box9">box9</div>
    </div>
   )
 }
