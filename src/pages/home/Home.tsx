dee7fb5c09f5d3d4f90185f653df22ae67791153
Johnwav <johnwav@users.noreply.github.com>
2023-09-11 23:44:41 +0100

    Home init

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,9 +3,17 @@ import './home.scss'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+   <div className="home">
+    <div className="box box1">box1</div>
+    <div className="box box2">box2</div>
+    <div className="box box3">box3</div>
+    <div className="box box4">box4</div>
+    <div className="box box5">box5</div>
+    <div className="box box6">box6</div>
+    <div className="box box">box</div>
+    <div className="box box">box</div>
+    <div className="box box">box</div>
+   </div>
   )
 }
 
